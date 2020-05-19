156f9904442a9ca0f495efe9e29acc4f4de2c322
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2020-05-19 16:03:59 -0600

    Changing default max-compression size to 16 bits.

--- src/lzw.lisp
+++ src/lzw.lisp
@@ -12,24 +12,24 @@
 (defun compress-file (file)
   "Compresses a file into an LWZ encoded file with .Z extension."
   (let ((out-file (concatenate 'string file ".Z")))
-    (write-bytes-to-file (compress (read-file-bytes-to-list file 8)) out-file 12)))
+    (write-bytes-to-file (compress (read-file-bytes-to-list file 8)) out-file 16)))
 
 (defun decompress-file (file)
   "Decompresses an encoded LWZ file without the .Z extension."
   (let ((out-file (string-right-trim ".Z" file)))
-    (write-bytes-to-file (decompress (read-file-bytes-to-list file 12)) out-file 8)))
+    (write-bytes-to-file (decompress (read-file-bytes-to-list file 16)) out-file 8)))
 
 (defun compress (input-bytes)
   "Takes a list of bytes and encodes them into compressed LZW format."
-  (compress-algorithm (init-dict) 258 input-bytes nil))
+  (compress-algorithm (init-dict) #x102 input-bytes nil))
 
 (defun decompress (input-bytes)
   "Takes a list of compressed LZW encoded bytes and decompresses them into their original format."
-  (decompress-algorithm (init-d-dict) 258 input-bytes nil))
+  (decompress-algorithm (init-d-dict) #x102 input-bytes nil))
 
 (defun init-dict ()
   (let ((dict (make-hash-table :test 'equal))
-        (codes (loop for code from 0 to 255 collect code)))
+        (codes (loop for code from 0 to #xFF collect code)))
     (dolist (code codes)
       (add-to-dict (list code) code dict))
     dict))
@@ -37,7 +37,7 @@
 ;;; this is basically like init-dict except with the keys and values reversed for decompression
 (defun init-d-dict ()
   (let ((dict (make-hash-table :test 'eql))
-        (codes (loop for code from 0 to 255 collect code)))
+        (codes (loop for code from 0 to #xFF collect code)))
     (dolist (code codes)
       (add-to-dict code (list code) dict))
     dict))
@@ -66,8 +66,8 @@
           (t nil))))
 
 (defun compress-algorithm (dict current-code input-bytes output-bytes)
-  (cond ((> current-code 4095)
-         (compress-algorithm (init-dict) 258 input-bytes (cons 256 output-bytes)))
+  (cond ((> current-code #xFFFF)
+         (compress-algorithm (init-dict) #x102 input-bytes (cons #x100 output-bytes)))
         (t
          (if input-bytes
              (destructuring-bind (byte . rest) input-bytes
@@ -78,7 +78,7 @@
                        (compress-algorithm dict (1+ current-code) rest (cons byte output-bytes))
                        (let ((next-seq (cons new-code (cdr rest))))
                          (compress-algorithm dict current-code next-seq output-bytes))))))
-             (reverse (cons 257 output-bytes))))))
+             (reverse (cons #x101 output-bytes))))))
 
 (defun decompress-algorithm (dict current-code input-bytes output-bytes)
   (if input-bytes
@@ -86,23 +86,24 @@
         (let* ((byte (decode-byte encoded-byte dict))
                (next-encoded-byte (car rest))
                (next-byte (list (car (decode-byte next-encoded-byte dict)))))
-          (if (and (/= encoded-byte 256) (/= encoded-byte 257) (not (equal next-byte '(nil))))
+          (if (and (/= encoded-byte #x100 #x101) (car next-byte))
               (add-to-dict current-code (append byte next-byte) dict)
               (and byte (update-d-dict current-code byte dict)))
-          (cond ((= encoded-byte 256)
-                 (decompress-algorithm (init-d-dict) 258 rest output-bytes))
-                ((= encoded-byte 257)
+          (cond ((= encoded-byte #x100)
+                 (decompress-algorithm (init-d-dict) #x102 rest output-bytes))
+                ((= encoded-byte #x101)
                  (decompress-algorithm dict current-code rest output-bytes))
                 (t
                  (decompress-algorithm dict (1+ current-code) rest (cons byte output-bytes))))))
       (apply #'append (reverse output-bytes))))
 
 (defun write-bytes-to-file (bytes file-path bits)
-  (with-open-file (stream file-path :direction :output
-                           :if-does-not-exist :create
-                           :if-exists :supersede
-                           :element-type (list 'unsigned-byte bits))
-     (dolist (b (loop for b in bytes collect b))
+  (with-open-file (stream file-path
+                          :direction :output
+                          :if-does-not-exist :create
+                          :if-exists :supersede
+                          :element-type (list 'unsigned-byte bits))
+     (dolist (b bytes)
        (write-byte b stream))))
 
 (defun read-file-bytes-to-list (file-path bits)
