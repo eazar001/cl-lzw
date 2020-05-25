74467196a4a5b260d84a11ad4e750ea9bc9975d7
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2020-05-24 20:05:17 -0600

    Fixing unit-test scaffolding in asdf build.

--- cl-lzw.asd
+++ cl-lzw.asd
@@ -2,13 +2,15 @@
   :description "Implementation of LZW compression algorithm."
   :author "Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>"
   :license "MIT"
-  :pathname "src"
+  :pathname "src/"
   :components ((:file "lzw"))
   :in-order-to ((test-op (test-op "cl-lzw/tests"))))
 
 (defsystem #:cl-lzw/tests
   :depends-on (#:cl-lzw :fiveam)
-  :pathname "tests"
-  :components ((:file "package")
-               (:file "tests"))
-  :perform (test-op (o s) (uiop:symbol-call :fiveam :run! 'cl-lzw-tests:all-tests)))
+  :components ((:module "tests"
+                        :serial t
+                        :components ((:file "package")
+                                     (:file "tests"))))
+  :perform (test-op (o s)
+                    (symbol-call :fiveam :run! (find-symbol "ALL-TESTS" "CL-LZW-TESTS"))))
--- tests/tests.lisp
+++ tests/tests.lisp
@@ -1,41 +1,50 @@
 (in-package #:cl-lzw-tests)
 
 (def-suite all-tests
-    :description "Main tests for compression API")
+    :description "Unit-tests for compression/decompression API")
 
 (in-suite all-tests)
 
-(defun test-api ()
-  (run! 'all-tests))
+(defmacro with-teardown (path &body body)
+  (let ((a (concatenate 'string path "-compressed.txt"))
+        (b (concatenate 'string path "-decompressed.txt")))
+    `(progn ,@body
+            (if (probe-file ,a)
+                (delete-file ,a))
+            (if (probe-file ,b)
+                (delete-file ,b)))))
 
 (def-test compress-don-quixote ()
-  (cl-lzw:compress-file "../tests/text-files/don-quixote.txt" "../tests/text-files/don-quixote-compressed.txt")
+  (cl-lzw:compress-file "tests/text-files/don-quixote.txt" "tests/text-files/don-quixote-compressed.txt")
   (cl-lzw:decompress-file
-   "../tests/text-files/don-quixote-compressed.txt"
-   "../tests/text-files/don-quixote-decompressed.txt")
-  (let ((decompressed-bytes (read-file-bytes-to-list "../tests/text-files/don-quixote-decompressed.txt" 8))
-        (original-bytes (read-file-bytes-to-list "../tests/text-files/don-quixote.txt" 8)))
-    (is (equal decompressed-bytes original-bytes))))
+   "tests/text-files/don-quixote-compressed.txt"
+   "tests/text-files/don-quixote-decompressed.txt")
+  (with-teardown "tests/text-files/don-quixote"
+    (let ((decompressed-bytes (read-file-bytes-to-list "tests/text-files/don-quixote-decompressed.txt" 8))
+          (original-bytes (read-file-bytes-to-list "tests/text-files/don-quixote.txt" 8)))
+      (is (equal decompressed-bytes original-bytes)))))
 
 (def-test compress-moby-dick ()
-  (cl-lzw:compress-file "../tests/text-files/moby-dick.txt" "../tests/text-files/moby-dick-compressed.txt")
+  (cl-lzw:compress-file "tests/text-files/moby-dick.txt" "tests/text-files/moby-dick-compressed.txt")
   (cl-lzw:decompress-file
-   "../tests/text-files/moby-dick-compressed.txt"
-   "../tests/text-files/moby-dick-decompressed.txt")
-  (let ((decompressed-bytes (read-file-bytes-to-list "../tests/text-files/moby-dick-decompressed.txt" 8))
-        (original-bytes (read-file-bytes-to-list "../tests/text-files/moby-dick.txt" 8)))
-    (is (equal decompressed-bytes original-bytes))))
+   "tests/text-files/moby-dick-compressed.txt"
+   "tests/text-files/moby-dick-decompressed.txt")
+  (with-teardown "tests/text-files/moby-dick"
+    (let ((decompressed-bytes (read-file-bytes-to-list "tests/text-files/moby-dick-decompressed.txt" 8))
+          (original-bytes (read-file-bytes-to-list "tests/text-files/moby-dick.txt" 8)))
+      (is (equal decompressed-bytes original-bytes)))))
 
 (def-test compress-alice-in-wonderland ()
   (cl-lzw:compress-file
-   "../tests/text-files/alice-in-wonderland.txt"
-   "../tests/text-files/alice-in-wonderland-compressed.txt")
+   "tests/text-files/alice-in-wonderland.txt"
+   "tests/text-files/alice-in-wonderland-compressed.txt")
   (cl-lzw:decompress-file
-   "../tests/text-files/alice-in-wonderland-compressed.txt"
-   "../tests/text-files/alice-in-wonderland-decompressed.txt")
-  (let ((decompressed-bytes (read-file-bytes-to-list "../tests/text-files/alice-in-wonderland-decompressed.txt" 8))
-        (original-bytes (read-file-bytes-to-list "../tests/text-files/alice-in-wonderland.txt" 8)))
-    (is (equal decompressed-bytes original-bytes))))
+   "tests/text-files/alice-in-wonderland-compressed.txt"
+   "tests/text-files/alice-in-wonderland-decompressed.txt")
+  (with-teardown "tests/text-files/alice-in-wonderland"
+    (let ((decompressed-bytes (read-file-bytes-to-list "tests/text-files/alice-in-wonderland-decompressed.txt" 8))
+          (original-bytes (read-file-bytes-to-list "tests/text-files/alice-in-wonderland.txt" 8)))
+      (is (equal decompressed-bytes original-bytes)))))
 
 (defun read-file-bytes-to-list (file-path bits)
   (with-open-file (stream file-path
